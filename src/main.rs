8723b415009c125f05213c7db5c8e13816f53ed3
Ankur Rathore <ankur@intuitorit.com>
2022-02-07 11:58:59 +0530

    added modularity and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
-
+use std::env;
+use std::process;
+use minigrep::Config;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
 
-    println!("Hello, world!");
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        println!("Problem parsing arguments {}",err);
+        process::exit(1);
+    });
+
+    // println!("{:?}",args);
+    println!("Searching for {}",config.query);
+    println!("In file {}",config.filename);
+
+    if let Err(e) = minigrep::run(config){
+        println!("Application errors:{}",e);
+        process::exit(1);
+    }
 }
+
+
+
